7e56e02c33139f1e48e4b3dc6e40bcd9f6db4d36
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-08-07 22:45:13 -0500

    add contact endpoint

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const express = require('express')
 
 const app = express()
 
+const nodemailer = require('nodemailer');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const cors = require('cors');
+app.use(cors());
+
+// body parser middleware
+app.use(express.json());
+
 const PORT = process.env.PORT || 8080
 
 // serve react app from client build folder
@@ -11,6 +22,44 @@ app.use(express.static('client/build'))
 // hello team endpoint
 app.get('/api/hello', (req, res) => {
     res.send({ express: 'Hello from Express' })
+})
+
+// POST - /api/contact - sends an email - PUBLIC
+app.post('/api/contact', (req, res) => {
+
+    let { from, subject, message } = req.body;
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD,
+        },
+    });
+
+    let mailOptions = {
+        from: from,
+        to: process.env.EMAIL_USERNAME,
+        subject: subject,
+        text: message,
+    };
+
+    try {
+        transporter.sendMail(mailOptions, (err, data) => {
+            if (err) {
+                console.log('Error occurs', err);
+            } else {
+                console.log('Email sent!!!');
+            }
+        });
+
+    } catch (error) {
+        throw error;
+    }
+    res.json({
+        status: 'success',
+        message: 'Email sent'
+    })
 })
 
 app.listen(PORT, () => {
