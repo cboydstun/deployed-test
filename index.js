c2c1a6bff8186c1222585372c27216caa667a411
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-08-07 22:10:49 -0500

    added an endpoint

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const PORT = process.env.PORT || 8080
 // serve react app from client build folder
 app.use(express.static('client/build'))
 
+// hello team endpoint
+app.get('/api/hello', (req, res) => {
+    res.send({ express: 'Hello from Express' })
+})
+
 app.listen(PORT, () => {
     console.log(`Server listening on: ${PORT}`)
 })
